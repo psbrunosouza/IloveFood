0992ad6db566af08b0553f20c78978795151ea5a
psbrunosouza <psbrunosouza@gmail.com>
2021-07-29 20:00:42 -0300

    home page created

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,10 +2,11 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 import { ComponentsModule } from '@components/@components.module';
 import { AppComponent } from './app.component';
+import { HomeModule } from './pages/home/home.module';
 
 @NgModule({
   declarations: [AppComponent],
-  imports: [BrowserModule, ComponentsModule],
+  imports: [BrowserModule, ComponentsModule, HomeModule],
   providers: [],
   bootstrap: [AppComponent],
 })
