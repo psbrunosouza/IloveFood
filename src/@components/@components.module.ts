51cefcbbf65d89ff16a4729c5c6c480c7975d005
psbrunosouza <psbrunosouza@gmail.com>
2021-07-29 00:32:16 -0300

    menu implemented

--- src/@components/@components.module.ts
+++ src/@components/@components.module.ts
@@ -2,13 +2,15 @@ import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { HeaderTemplateComponent } from './header-template/header-template.component';
 import { FooterTemplateComponent } from './footer-template/footer-template.component';
+import { MenuComponent } from './menu/menu.component';
 
 
 
 @NgModule({
   declarations: [
     HeaderTemplateComponent,
-    FooterTemplateComponent
+    FooterTemplateComponent,
+    MenuComponent
   ],
   imports: [
     CommonModule
