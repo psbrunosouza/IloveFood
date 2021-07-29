0c77c972b07dd342f7726aed9466e81db9655ef3
psbrunosouza <psbrunosouza@gmail.com>
2021-07-29 18:57:02 -0300

    main menu and interface created

--- src/@components/@components.module.ts
+++ src/@components/@components.module.ts
@@ -2,7 +2,7 @@ import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { HeaderTemplateComponent } from './header-template/header-template.component';
 import { FooterTemplateComponent } from './footer-template/footer-template.component';
-import { MenuComponent } from './menu/menu.component';
+import { MenuComponent } from './header-template/components/menu/menu.component';
 
 
 
