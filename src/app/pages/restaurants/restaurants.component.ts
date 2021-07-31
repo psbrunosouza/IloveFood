35f9a0e87c62cc43874f75a8d22b0537c0d8100d
psbrunosouza <psbrunosouza@gmail.com>
2021-07-31 13:08:22 -0300

    restaurants interface applied

--- src/app/pages/restaurants/restaurants.component.ts
+++ src/app/pages/restaurants/restaurants.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { Restaurant } from '@core/data/restaurant';
 
 @Component({
   selector: 'lov-restaurants',
@@ -6,6 +7,25 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./restaurants.component.scss']
 })
 export class RestaurantsComponent implements OnInit {
+
+  restaurants: Restaurant[] = [
+    {
+      id: "bread-bakery",
+      name: "Bread & Bakery",
+      category: "Padaria",
+      deliveryEstimate: "25m",
+      rating: 4.9,
+      imagePath: "assets/img/restaurants/breadbakery.png",
+    },
+    {
+      id: "burger-house",
+      name: "Burger House",
+      category: "Hamburgers",
+      deliveryEstimate: "100m",
+      rating: 3.5,
+      imagePath: "assets/img/restaurants/burgerhouse.png",
+    },
+  ];
 
   constructor() { }
 
